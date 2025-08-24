b330b25bd3def578f1bc633a6550654f0bec62a5
Rahul Autade <rahulsautade@gmail.com>
2025-08-24 16:49:45 -0500

    tuning

--- src/inferenceworker.h
+++ src/inferenceworker.h
@@ -7,8 +7,8 @@
 #include <onnxruntime_cxx_api.h>
 
 
-#define CONFIDENCE_THRESHOLD 0.05f
-#define OVERLAP_THRESHOLD 0.05f
+#define CONFIDENCE_THRESHOLD 0.2f
+#define OVERLAP_THRESHOLD 0.2f
 #define TILE_FACTOR 4
 
 class InferenceWorker : public QObject {
